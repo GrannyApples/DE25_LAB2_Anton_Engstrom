32b287d8ec85d82be560817aa29da73b4a7475ee
GrannyApples <anton.engstrom1994@gmail.com>
2025-10-31 10:48:12 +0100

    test:circle equality

--- rectangle.py
+++ rectangle.py
@@ -2,6 +2,12 @@ from shape import Shape
 
 class Rectangle(Shape):
     ##This autocompleted with intellisense, so just need to add height and width, will fix after circle is done.
-    def __init__(self, x=0, y=0):
+    def __init__(self, x=0, y=0, width=1, height=1):
         super().__init__(x, y)
-        
+        try:
+            self.width = float(width)
+            self.height = float(height)
+        except:
+            raise TypeError("width and height must be numbers")
+        if self.width <= 0 or self.height <= 0:
+            raise ValueError("width and height must be positive")
--- test_circle.py
+++ test_circle.py
@@ -15,3 +15,10 @@ def test_circle_area():
 def test_circle_perim():
     c=Circle(0,0,2)
     assert round(c.perimeter, 2)==12.57
+
+def test_circle_eq():
+    c1=Circle(0,0,1)
+    c2=Circle(1,1,1)
+    assert c1==c2
+    c3=Circle(0,0,2)
+    assert c1 != c3
